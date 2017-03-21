29bca065f045a46c8677635944c2295f0f8bc10c
Yuan711 <yuan@hust.edu.cn>
2017-03-21 09:57:22 +0800

    Bing-Task1

---
+++ Bing-Task1/javascript1.js
@@ -0,0 +1,29 @@
+function addLoadEvent(func){
+  var oldOnload=window.onload;
+  if(typeof window.onload!='function'){
+    window.onload=func;
+  }
+  else{
+    window.onload=function(){
+      oldOnload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(airQuality);
+
+function airQuality(){
+	var airButton=document.getElementById("button");
+	airButton.onclick=function(){
+    displayQuality();
+  }
+}
+
+function displayQuality(){
+	var aqiInput=document.getElementById("aqi-input").value;
+	document.getElementById("aqi-input").value="";
+	var aqiDisplay=document.getElementById("aqi-display");
+	aqiDisplay.innerHTML=aqiInput;
+}
+	
